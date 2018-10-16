8eeb0f31ba431c96e17ef90c35baeffa36fee0c0
Gyumeijie <18170879819@163.com>
2018-10-16 22:45:00 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ViPages
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT) 
+
+Browse web pages with vi(m)-like shortcuts.
+
+# Current supported shortcuts
+
+- gg            
+scroll to top 
+- G            
+scroll to bottom
+- j            
+downwards scroll one page height
+- k            
+upwards scroll one page height
---
+++ vipages.js
@@ -0,0 +1,44 @@
+(function(c, d){
+   
+   // Initialize keymaps for 26 letters
+   var keymaps = {};
+   var index = 65;
+   "abcdefghijklmnopqrstuvwxyz".split('').map(function(item) {
+      keymaps[item]= index++;
+   })
+
+   var onePageHeight = window.innerHeight;
+   var keystrokes = 0;
+
+   function keyHandler(e) {
+      // If the active element is not body, say 'input' element, we should do nothing
+      if (document.activeElement.tagName !== 'BODY') return;
+    
+      keystrokes += 1;
+      if (e.which !== keymaps['g']) keystrokes = 0;
+
+      if (e.which === keymaps['j']) {
+         // Downwards scroll one page height
+         window.scrollBy(0, onePageHeight);
+      } else if ( e.which === keymaps['k']) {
+         // Upwards scroll one page height
+         window.scrollBy(0, -onePageHeight);
+      } else if ( e.which === keymaps['g']) {
+         // Scroll to bottom
+         if (e.getModifierState('CapsLock')) {
+            window.scrollTo(0, document.body.scrollHeight);
+         } else {
+            if (keystrokes === 2) {
+               // If two 'g' was stroked continuously, then scroll to top
+               keystrokes = 0;
+               window.scrollTo(0, 0);
+            }
+         }
+      }
+   }
+  
+   d.addEventListener('keydown', keyHandler, false);
+
+})(chrome, document);
+
+
